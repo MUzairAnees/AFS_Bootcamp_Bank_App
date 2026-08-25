5d04c2a42c025f04da5d6c9a34cc559e053e2ed9
Mohamed Uzair Anees <mohameduanees.cse@outlook.com>
2026-08-24 20:55:07 -0500

    created exceptions.py

---
+++ bankapp/exceptions.py
@@ -0,0 +1,21 @@
+class BankError(Exception):
+    '''
+    Bank Error(Exception):
+    -Whenever something goes wrong/any Exception occurs within the banking app, it is expressed as a BankError.
+    '''
+
+class InsufficientFundsError(BankError):
+    '''
+    InsufficientFundsError(BankError):
+    -InsufficientFundsError extends the BankError class.
+    -So this IS a BankError but a specific kind, raised in a specific situation only.
+    -This specific exception should be raised when a user tries to withdraw more money
+    than they have left in their Savings Account.
+    '''
+
+class NotFoundError(BankError):
+    '''
+    NotFoundError(BankError):
+    -NotFoundError extends the BankError class.
+    -This is a BankError but a specific kind, raised in a specific situation only.
+    '''
