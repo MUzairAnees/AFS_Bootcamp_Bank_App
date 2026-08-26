0e7f71ed5e8a200c01e981f9fa20dc69668b995a
Mohamed Uzair Anees <mohameduanees.cse@outlook.com>
2026-08-25 21:32:34 -0500

    updated admin dashboard in main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from bankapp.user import User, Customer, Admin, USERS
+from bankapp.bank import Bank, create_bank
+from bankapp.user import User, Customer, Admin
+from bankapp.exceptions import *
 
 def main():
     '''
@@ -6,6 +8,7 @@ def main():
     Acts as the entry point of the program.
 
     Purpose:
+    0) Creates a bank to work with.
     1) Welcomes user -> welcome().
     2) Prompts for authorization -> login().
     2.a) If authorization returns a specific val(stored in check var)
@@ -13,30 +16,37 @@ def main():
     3) Exits program -> goodbye().
     '''
 
+    bank = create_bank()
+
     welcome()
 
     #login() -> contains login logic and stores results in var
-    login_res = login()
+    login_res = login(bank)
 
     if isinstance(login_res, Admin):
-        admin_dashboard(login_res)
+        admin_dashboard(bank, login_res)
     elif isinstance(login_res, Customer):
-        customer_dashboard(login_res)
+        customer_dashboard(bank, login_res)
     else:
         print("\nLog in failed. Please try again later.")
         # moves to goodbye()
 
     goodbye()
 
+
+#------------------------------------------------------------------------#
+
+
 def welcome():
     '''
     Welcome():
     Prints welcome message
     '''
+    print("---------------------------")
+    print("--Welcome to AFS Banking!--")
+    print("---------------------------\n")
 
-    print("AFS Bank welcomes you!")
-
-def login() -> User | None:
+def login(bank: Bank) -> User | None:
     '''
     Login():
     Asks user for username and password.
@@ -46,27 +56,140 @@ def login() -> User | None:
     username = input("Please enter your username: ")
     password = input("Please enter your password: ")
 
-    #return the object
-    for user in USERS:
-        if user.username == username and user.password == password:
-            return user
+    return bank.login(username, password)
 
-    #or return None
-    return None
-
-def admin_dashboard(admin: Admin):
+def admin_dashboard(bank: Bank, admin: Admin):
     '''
     AdminDashboard():
     Contains a menu of what the admin can do.
     '''
     print(f"\nHello {admin.username}.")
 
-def customer_dashboard(customer: Customer):
+    while True:
+        choice = input("\nWhat would you like to do?\n"
+                       "1) View all customers\n"
+                       "2) View all accounts\n"
+                       "3) Add a customer\n"
+                       "4) Delete a customer\n"
+                       "5) Update a customer\n"
+                       "6) Exit\n"
+                       "Enter your choice: ")
+
+        match choice:
+            case "1":
+                #Views bank users -> customers
+                for user in bank.users:
+                    if isinstance(user, Customer):
+                        print(user)
+
+            case "2":
+                #Views all bank accounts
+                for account in bank.accounts:
+                    print(account)
+
+            case "3":
+                #Adds customer based on customerID -> only if customerID doesn't exist.
+                customer_id = int(input("Please enter customer id: "))
+                try:
+                    bank.find_customer(customer_id)
+                    print("Customer ID already exists.")
+                except NotFoundError:
+                    name = input("Please enter customer name: ")
+                    email = input("Please enter customer email: ")
+                    branch_id = int(input("Please enter customer branch id: "))
+                    username = input("Please enter customer username: ")
+                    password = input("Please enter customer password: ")
+
+                    new_customer = Customer(customer_id, name, email, branch_id, username, password)
+                    bank.add_user(new_customer)
+                    print(f"\nCustomer {name}, {customer_id} added.")
+
+            case "4":
+                customer_to_delete_id = int(input("Please enter customer id: "))
+                try:
+                    bank.remove_customer(customer_to_delete_id)
+                    print("\nCustomer deleted.")
+                except NotFoundError:
+                    print("Customer does not exist.")
+
+            case "5":
+                customer_id = int(input("Please enter customer id: "))
+                try:
+                    customer_to_update = bank.find_customer(customer_id)
+                except NotFoundError:
+                    print("Customer does not exist.")
+                else:
+                    print(f"Editing {customer_to_update.customer_id}: {customer_to_update.name}.")
+
+                    print("\nWhat would you like to update?\n"
+                          "1) Name\n"
+                          "2) Email\n"
+                          "3) Branch ID\n"
+                          "4) Username\n"
+                          "5) Password\n")
+                    choice = input("Please enter your choice: ")
+
+                    match choice:
+                        case "1":
+                            customer_to_update.name = input("Please enter new customer name: ")
+                            print("\nCustomer updated.")
+                        case "2":
+                            customer_to_update.email = input("Please enter new customer email: ")
+                            print("\nCustomer updated.")
+                        case "3":
+                            customer_to_update.branch_id = int(input("Please enter new customer branch id: "))
+                            print("\nCustomer updated.")
+                        case "4":
+                            customer_to_update.username = input("Please enter new customer username: ")
+                            print("\nCustomer updated.")
+                        case "5":
+                            customer_to_update.password = input("Please enter new customer password: ")
+                            print("\nCustomer updated.")
+                        case _:
+                            print("Invalid choice. Please try again.")
+
+            case "6":
+                print("\nExiting dashboard...")
+                break
+
+            case _:
+                print("\nInvalid choice. Please try again.")
+
+def customer_dashboard(bank: Bank, customer: Customer):
     '''
     CustomerDashboard():
     Contains a menu of what the customer can do.
     '''
     print(f"\nHello {customer.name}!")
+
+    while True:
+        choice = input("\nWhat would you like to do?\n"
+                       "1) View your accounts\n"
+                       "2) Deposit an amount\n"
+                       "3) Withdraw an amount\n"
+                       "4) Transfer an amount between accounts\n"
+                       "5) Exit\n"
+                       "Enter your choice: ")
+
+        match choice:
+            case "1":
+                if not customer.accounts:
+                    print("No accounts created.")
+                else:
+                    for account in customer.accounts:
+                        print(account)
+            case "2":
+                print("[TODO] update a specific account object's value -> adding amount")
+
+            case "3":
+                print("[TODO] update a specific account object's value -> deleting amount based on account object rules")
+            case "4":
+                print("[TODO] update a specific account object's value -> withdraw amount -> deposit amount into a specific account's value")
+            case "5":
+                print("\nExiting dashboard...")
+                break
+            case _:
+                print("\nInvalid choice. Please try again.")
 
 def goodbye():
     '''
@@ -74,7 +197,9 @@ def goodbye():
     Prints goodbye message.
     Final call of the program.
     '''
-    print("\nThank you for using AFS Bank!")
+    print("\n---------------------------------")
+    print("--Thank you for using AFS Bank!--")
+    print("---------------------------------")
 
 if __name__ == '__main__':
     main()
